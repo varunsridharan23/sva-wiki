cdcd80937b6cf01cc3ee71ee38608873fcf31d93
Varun Sridharan <varunsridharan23@gmail.com>
2021-03-25 12:22:29 +0530

    Update generate.php

--- .github/generate.php
+++ .github/generate.php
@@ -1,5 +1,6 @@
 <?php
 $output = <<<HTML
+<meta name="monetization" content="$ilp.uphold.com/edRH7RxRBhXz"/>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
 	  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 	  <div class="container-fluid py-3">
